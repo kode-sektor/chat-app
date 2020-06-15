dc80f20cd6e30936ff33edbe1146529463ffd1d1
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-06-15 19:06:43 -0400

    Put new routes in entry file

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-const express  = require('express')
-const app      = express()
-const port     = 3000
-const server   = app.listen(port, () => {console.log(`Server running at http://localhost:${port}`)})
-const io 			 = require('socket.io').listen(server)
+const express  = require('express');
+const app      = express();
+const port     = 5001;
+const server   = app.listen(port, () => {console.log(`Server running at http://localhost:${port}`)});
+const io 	   = require('socket.io').listen(server);
 
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.use(express.static('.'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/wddm-121.html');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/wddm-122.html');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/wddm-123.html');
+});
+
+
+
+app.use(express.static('.'));
 
 
 
@@ -19,18 +33,18 @@ io.on('connection', function (socket) {
   // Listen for a "newuser" message
   socket.on('newuser', (data) => {
     // Transmit a message to everyone except the sender
-    socket.broadcast.emit('newuser', data)
+    socket.broadcast.emit('newuser', data);
 
     // The same message, sent to all users - try it!
     //io.emit('newuser', data)
-	})
+	});
 	 
 	
 	// Listen for "chatmsg"
 	//   io.emit to all user
 	socket.on('chatmsg', (data) => {
-		io.emit('chatmsg', data)
-	})
+		io.emit('chatmsg', data);
+	});
 
 
-})
+});
