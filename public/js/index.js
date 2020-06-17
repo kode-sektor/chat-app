ac552363dd4ced2aa627d61760f755fda80ac891
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-06-16 20:53:16 -0400

    Complete logic for 'me vs they' and annotate all index.js codes

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,32 @@
 'use strict';
 
 const timeHumanise = () => {
-	let date = new Date();
-	let day = date.getDate();	
+	let date = new Date();	// get date now
+	let day = date.getDate();	// get day
 
-	let hr = date.getHours();
-	let min = date.getMinutes();
-	let sec = date.getSeconds();
+	let hr = date.getHours();	// hours
+	let min = date.getMinutes();	// mins
+	let sec = date.getSeconds();	// secs 
+
+	// Prefix with '0' if second is less than 10
 	(sec) = (sec.toString().length == '1') ? ('0' + sec) : sec;
 	return `<time class='chat-stamp' datetime='${hr}-${min}-${sec}'>${hr}:${min}:${sec}</time>`;
 }
 
 const sanitizeHTML = function (str) {
-	let temp = document.createElement('div');
-	temp.textContent = str;
-	return temp.innerHTML;
+	let temp = document.createElement('div');	// create new div
+	temp.textContent = str;	// populate with string using safe 'textContent' JS set property
+	return temp.innerHTML;	// return stripped HTML
 };
 
 const logged = ({msgList, state, name}) => {
-
+	// check if it is 'connected' or 'disconnected' event that was fired
 	let connected = (state === 'online') ? 'connected' : 'disconnected';
 
-	const newMsg = document.createElement('li');
-	newMsg.classList.add('joined');
-	msgList.appendChild(newMsg);
+	const newMsg = document.createElement('li');	// create new li to append
+	newMsg.classList.add('joined');	// style
+	msgList.appendChild(newMsg);	// append HTML 
+	// Display logged message
 	newMsg.innerHTML= `<span><span class="other-user">${name}</span> has ${connected}</span>`;	
 }
 
@@ -62,21 +65,26 @@ const connect = (name, chatRoom, moniker) => {
 	});
 
 	// Listen to submission of chat and then emit message in room
-	$msgForm.addEventListener('submit', (e) => {
-		e.preventDefault();
+	$msgForm.addEventListener('submit', (e) => { 
+		e.preventDefault();	// prevent default action
 
-		let chatMsg = sanitizeHTML(($textbox.value).trim());
-		socket.emit('message', {chatMsg, room});
-		$textbox.value = '';
+		// neutralise XSS attack and eliminate unnecessary whitespace
+		let chatMsg = sanitizeHTML(($textbox.value).trim());	
+		socket.emit('message', {chatMsg, room});	// emit message 
+		$textbox.value = '';	// clear textbox
 
 	});
 
 	socket.on('message', (msg)=> {
-		if (meVsThey(msg.name)) alert ('true');
-		const newMsg = document.createElement('li');
-		newMsg.classList.add('msg');
-		$msgList.appendChild(newMsg);
-		newMsg.innerHTML= `<span class="other-user">${msg.name}: </span>  ${msg.message} - ${timeHumanise()}`;
+		// compare if the username emitted is the same as that which was collected
+		// from user input in dialogue modal form
+		let me = (meVsThey(msg.name)) ? 'other-' : '';	// create new class for others
+
+		const newMsg = document.createElement('li');	// create li tag
+		newMsg.classList.add('msg');	// insert message
+		$msgList.appendChild(newMsg);	// append message
+		// append in human readable format
+		newMsg.innerHTML= `<span class="${me}user">${msg.name}: </span>  ${msg.message} - ${timeHumanise()}`;
 	});
 
 }
